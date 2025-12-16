ed58a1d4aed51eb733afebbf1d7e70d229809b6a
Daya sharma <Dayasharma4125@gmail.com>
2025-12-16 21:16:36 +0530

    your comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,16 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
+import { Router as WouterRouter } from "wouter";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={import.meta.env.BASE_URL}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
